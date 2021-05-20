ccb85b6dca1fd5fec4766936c7055edcc9e0d3b2
Mykola Luniv <lunivm@yahoo.com>
2021-05-20 13:07:23 +0300

    add universal transfer http cache module

--- apps/ng-seo/src/app/app.module.ts
+++ apps/ng-seo/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TransferHttpCacheModule } from '@nguniversal/common';
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    TransferHttpCacheModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
