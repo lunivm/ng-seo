856e743c8fd3649c87569a475f6d25cf224acce0
Mykola Luniv <lunivm@yahoo.com>
2021-05-20 12:56:01 +0300

    add logging

--- apps/ng-seo/src/app/search/search.component.ts
+++ apps/ng-seo/src/app/search/search.component.ts
@@ -8,14 +8,17 @@ import { SearchService } from './search.service';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  items: SearchItem[] = [];
+  items: SearchItem[] =[];
 
   constructor(
     private readonly searchService: SearchService
-  ) { }
+  ) {
+    console.log('SearchComponent');
+  }
 
   ngOnInit(): void {
     this.searchService.getSearch().subscribe(i => {
+      console.log('get search items');
       this.items = i.items;
     });
   }
